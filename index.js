0693ee0daab3824253b815051a721620998a50d8
Malo Germond <github@matchalo.org>
2026-02-10 19:36:27 +0100

    :art: able to load txt file

--- index.js
+++ index.js
@@ -2,13 +2,32 @@
 
 import { program } from "commander";
 import chalk from "chalk";
+import { readTxtFile } from "./src/utils/readFile.js";
 
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 const { version, description } = require("./package.json");
 
 // // Configuration de base
-program.name("rtfCleanner").description({ description }).version(version);
+program.name("rtfCleanner").description(description).version(version);
+
+// Commande pour lire un fichier TXT
+program
+    .command("read-txt")
+    .description("Lire le contenu d'un fichier TXT")
+    .argument("<path>", "Chemin vers le ficher")
+    .action(async (filePath, options) => {
+        console.log({ filePath }, { options });
+        try {
+            const content = await readTxtFile(filePath);
+            console.log(chalk.green("=== Contenu du fichier TXT ==="));
+            console.log(content);
+            console.log(chalk.green("=== Fin du contenu ==="));
+        } catch (error) {
+            console.error(chalk.red(`Erreur: ${error.message}`));
+            process.exit(1);
+        }
+    });
 
 try {
     program.parse(process.argv);
