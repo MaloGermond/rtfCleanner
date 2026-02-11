531138accd9f3fd51fec00f5e95ea2d801d552e4
Malo Germond <github@matchalo.org>
2026-02-11 21:51:51 +0100

    :art: Can load config file and rtf file

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 import { program } from "commander";
 import chalk from "chalk";
 import { convertTxtToRTF } from "./src/utils/convertFile.js";
+import { cleanRtf } from "./src/utils/rtfgrooming.js";
+import { readRtfFile, readJsonFile } from "./src/utils/readFile.js";
 
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
@@ -24,6 +26,27 @@ program
         console.log({ filePath }, { options });
         try {
             await convertTxtToRTF(filePath, options?.output);
+        } catch (error) {
+            console.error(chalk.red(`Erreur: ${error.message}`));
+            process.exit(1);
+        }
+    });
+
+program
+    .command("rtfClean")
+    .description("Format et nettoie de contenu du rtf")
+    .argument("<path>", "Chemin vers le fichier RTF à convertir")
+    .option(
+        "-o, --output <path>",
+        "Chemin vers le fichier RTF de sortie (facultatif)",
+    )
+    .option("-c, --config <path>", "Chemin vers le fichier de config")
+    .action(async (filePath, options) => {
+        console.log({ filePath }, { options });
+        try {
+            const content = await readRtfFile(filePath);
+            await readJsonFile(options?.config, true);
+            // await cleanRtf(content, );
         } catch (error) {
             console.error(chalk.red(`Erreur: ${error.message}`));
             process.exit(1);
--- src/utils/readFile.js
+++ src/utils/readFile.js
@@ -1,4 +1,5 @@
 import fs from "fs";
+import chalk from "chalk";
 
 /**
  * Lire le contenu d'un fichier TXT de manière synchrone
@@ -16,11 +17,57 @@ export function readTxtFile(filePath, verbose = false) {
         throw new Error(`File "${filePath}" is not a .txt file.`);
     }
 
+    const content = fs.readFile(filePath, "utf8");
+
+    if (verbose) {
+        console.log(chalk.green("=== Contenu du fichier TXT ==="));
+        console.log(content);
+        console.log(chalk.green("=== Fin du contenu ==="));
+    }
+
+    // Lire le contenu du fichier de manière synchrone
+    return content;
+}
+
+export function readRtfFile(filePath, verbose = false) {
+    // Vérifier si le fichier existe
+    if (!fs.existsSync(filePath)) {
+        throw new Error(`File "${filePath}" does not exist.`);
+    }
+
+    // Vérifier que c'est bien un fichier TXT
+    if (!filePath.toLowerCase().endsWith(".rtf")) {
+        throw new Error(`File "${filePath}" is not a .rtf file.`);
+    }
+
     const content = fs.readFileSync(filePath, "utf8");
 
     if (verbose) {
         console.log(chalk.green("=== Contenu du fichier TXT ==="));
         console.log(content);
+        console.log(chalk.green("=== Fin du contenu ==="));
+    }
+
+    // Lire le contenu du fichier de manière synchrone
+    return content;
+}
+
+export function readJsonFile(filePath, verbose = false) {
+    // Vérifier si le fichier existe
+    if (!fs.existsSync(filePath)) {
+        throw new Error(`File "${filePath}" does not exist.`);
+    }
+
+    // Vérifier que c'est bien un fichier TXT
+    if (!filePath.toLowerCase().endsWith(".json")) {
+        throw new Error(`File "${filePath}" is not a .json file.`);
+    }
+
+    const content = JSON.parse(fs.readFileSync(filePath, "utf8"));
+
+    if (verbose) {
+        console.log(chalk.green("=== Contenu du fichier TXT ==="));
+        console.log({ content });
         console.log(chalk.green("=== Fin du contenu ==="));
     }
 
