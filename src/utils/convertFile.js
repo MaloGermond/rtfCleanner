2743746a441826b8f7aa3cef376df8ea7e19c049
Malo Germond <github@matchalo.org>
2026-02-11 11:50:52 +0100

    :construction: Trying to convert by renaming... it failled

--- src/utils/convertFile.js
+++ src/utils/convertFile.js
@@ -1,11 +1,12 @@
 import fs from "fs";
+import path from "path";
 
 /**
  * Lire le contenu d'un fichier TXT de manière synchrone
  * @param {string} filePath - Chemin vers le fichier TXT
  * @returns {string} Contenu du fichier TXT
  */
-export function convertTxtToRTF(filePath, output) {
+export function convertTxtToRTF(filePath, outputDir = "./") {
     // Vérifier si le fichier existe
     if (!fs.existsSync(filePath)) {
         throw new Error(`File "${filePath}" does not exist.`);
@@ -17,5 +18,16 @@ export function convertTxtToRTF(filePath, output) {
     }
 
     // Lire le contenu du fichier de manière synchrone
-    return fs.readFileSync(filePath, "utf8");
+    const content = fs.readFileSync(filePath, "utf8");
+
+    const newFileName = path.basename(filePath, ".txt") + ".rtf";
+    const outputPath = path.join(outputDir, newFileName);
+
+    fs.writeFile(outputPath, content, (err) => {
+        if (err) {
+            console.error("Erreur lors de l'écriture du fichier :", err);
+            return;
+        }
+        console.log(`Fichier copié et renommé avec succès : ${outputPath}`);
+    });
 }
